e0ce51455b4800bed91308d0bc34343a1a36f5da
Paweł Walenda <pawel.walenda@gmail.com>
2020-12-28 22:44:23 +0100

    Completed module

--- ssg.py
+++ ssg.py
@@ -1,10 +1,11 @@
 import typer
+
 from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
-    Site(**config).build()
 
+    Site(**config).build()
 
 typer.run(main)
